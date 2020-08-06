a9d259553b967cb006527d1a726aa18db79d0431
Francesco <francesco@desegno.it>
2020-08-06 17:24:15 +0200

    Key value storage initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:scelteperte/home.dart';
+import 'package:scelteperte/src/providers/spt_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(AppSplashLoader());
 }
 
-Future<dynamic> fetchData() async {
-  final response =  await http.get('http://www.scelteperte.it/sptrapi/initialize?mtoken=FNmZbp6nR7EEs7Eg&ts=');
-  if (response.statusCode == 200) {
-    return jsonDecode(response.body);
-  } else {
-    throw Exception('Failed to load data');
-  }
-}
 
 class AppSplashLoader extends StatefulWidget {
   @override
@@ -23,67 +15,78 @@ class AppSplashLoader extends StatefulWidget {
 }
 
 class _AppSplashLoaderState extends State<AppSplashLoader> {
+
   Future<dynamic> appData;
+  Future<SharedPreferences> localStorage;
 
   @override
-  void initState() {
+  void initState(){
     super.initState();
-    appData = fetchData();
+    appData = SptProvider.fetchData();
+    localStorage = SptProvider.localStorage;
   }
 
   @override
   Widget build(BuildContext buildContext) {
-    return Container(
-      child: FutureBuilder<dynamic>(
-        future: fetchData(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
+    return FutureBuilder<SharedPreferences>(
+        future: localStorage,
+        builder: (context, AsyncSnapshot<SharedPreferences> storage) {
+          if(storage.hasData && storage.data.containsKey('boot') && storage.data.getBool('boot')){
             return Home();
-          } else if (snapshot.hasError) {
-
-            return Text("${snapshot.error}", textDirection: TextDirection.ltr,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black87));
-
           } else {
+            return FutureBuilder<dynamic>(
+                future: appData,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    storage.data.setBool('boot', true);
+                    return Home();
+                  } else if (snapshot.hasError) {
 
-            return DecoratedBox(
-              decoration: const BoxDecoration(color: Colors.white),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  textDirection: TextDirection.ltr,
-                  children: <Widget>[
-                    FlutterLogo(size: 48),
-                    Padding(
-                      padding: EdgeInsets.all(32),
-                      child: Text(
-                        'This app is only meant to be run under the Flutter debugger',
-                        textDirection: TextDirection.ltr,
+                    return Text("${snapshot.error}", textDirection: TextDirection.ltr,
                         textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.black87),
-                      ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        CircularProgressIndicator(),
-                        Text(
-                            'Caricamento Dati',
-                            textDirection: TextDirection.ltr,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.black87)
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            );
+                        style: TextStyle(color: Colors.black87));
 
+                  } else {
+
+                    return DecoratedBox(
+                      decoration: const BoxDecoration(color: Colors.white),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          textDirection: TextDirection.ltr,
+                          children: <Widget>[
+                            FlutterLogo(size: 48),
+                            Padding(
+                              padding: EdgeInsets.all(32),
+                              child: Text(
+                                'This app is only meant to be run under the Flutter debugger',
+                                textDirection: TextDirection.ltr,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.black87),
+                              ),
+                            ),
+                            Column(
+                              children: <Widget>[
+                                CircularProgressIndicator(),
+                                Text(
+                                    'Caricamento Dati',
+                                    textDirection: TextDirection.ltr,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(color: Colors.black87)
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+
+                  }
+                }
+            );
           }
         }
-      ),
-    );
+      );
   }
 }
 
--- lib/src/providers/spt_provider.dart
+++ lib/src/providers/spt_provider.dart
@@ -1,11 +1,23 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SptProvider {
 
+  /// K/V local app storage
+  static SharedPreferences _preferences;
+
   SptProvider._();
 
+  static Future<SharedPreferences> get localStorage async {
+    if (_preferences != null) return _preferences;
+    _preferences = await SharedPreferences.getInstance();
+    return _preferences;
+  }
+
   static Future<dynamic> fetchData() async {
+    final storage = await localStorage;
+
     final response =  await http.get('http://www.scelteperte.it/sptrapi/initialize?mtoken=FNmZbp6nR7EEs7Eg&ts=');
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
