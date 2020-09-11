7b7f778e8aa36f6236f74b2149322b7a784246e5
Francesco <francesco@desegno.it>
2020-09-11 17:31:03 +0200

    Detail e related for all entities

--- lib/home.dart
+++ lib/home.dart
@@ -155,7 +155,7 @@ class _HomeState extends State<Home> {
                                     log('tap');
                                   },
                                   child: MacroHomeSectionButton(
-                                      image: new NetworkImage(recipe.data.image),
+                                      image: new NetworkImage(recipe.data.thumb),
                                       title: 'Ricetta del mese'
                                   ));
                             } else {
